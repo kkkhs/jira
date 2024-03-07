bc9b62b98e1fc0b4ff0c23e50214e827b5ba7a83
khs <khs@123.com>
2024-03-07 12:01:39 +0800

    chore: 进行登陆页面请求

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -4,7 +4,7 @@ interface Project {
   id: string;
   name: string;
   personId: string;
-  pin: boolean;
+  created: number;
   organization: string;
 }
 
@@ -27,7 +27,7 @@ export const List = ({ list, users }: ListProps) => {
           <tr key={project.id}>
             <td>{project.name}</td>
             <td>
-              {users.find((user) => user.id === project.personId)?.name ||
+              {users.find((user) => user.id == project.personId)?.name ||
                 "未知"}
             </td>
           </tr>
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,9 +1,6 @@
 export interface User {
   id: string;
   name: string;
-  email: string;
-  title: string;
-  organization: string;
 }
 
 interface SearchPanelProps {
