1d88d06c5dabec12494dc9a8d24f341edaddfabc
khs <khs@123.com>
2024-03-07 18:47:43 +0800

    chore: 使用useContext存储用户全局信息

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useState } from "react";
 import * as auth from "auth-provider";
-import { User } from "../screens/project-list/search-panel";
+import { User } from "screens/project-list/search-panel";
 
 interface AuthForm {
   username: string;
@@ -21,6 +21,7 @@ AuthContext.displayName = "AuthContext";
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
 
+  // point free
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
@@ -29,7 +30,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     <AuthContext.Provider
       children={children}
       value={{ user, login, register, logout }}
-    ></AuthContext.Provider>
+    />
   );
 };
 
@@ -38,7 +39,5 @@ export const useAuth = () => {
   if (!context) {
     throw new Error("useAuth必须在AuthProvider中使用");
   }
-  return {
-    context,
-  };
+  return context;
 };
