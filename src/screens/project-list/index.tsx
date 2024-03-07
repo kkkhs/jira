5f4c46b40636fd5d24d0fa9c4844fed1a1a74181
khs <khs@123.com>
2024-03-07 20:46:46 +0800

    feat: 添加登陆状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,40 +1,38 @@
-import { useArray, useMount } from "utils";
-import React, { useState } from "react";
+import React from "react";
+import { SearchPanel } from "screens/project-list/search-panel";
+import { List } from "screens/project-list/list";
+import { useEffect, useState } from "react";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import * as qs from "qs";
+import { useHttp } from "../../utils/http";
+
+// 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
+// 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
-  const persons: { name: string; age: number }[] = [
-    { name: "jack", age: 25 },
-    { name: "ma", age: 22 },
-  ];
-  const { value, clear, removeIndex, add } = useArray(persons);
+  const [users, setUsers] = useState([]);
+
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+  const [list, setList] = useState([]);
+  const client = useHttp();
+
+  useEffect(() => {
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+  }, [debouncedParam]);
 
   useMount(() => {
-    // 期待这里报错：Property 'notExist' does not exist on type '{ name: string; age: number; }[]'.
-    // console.log(value.notExist);
-    //
-    // // 期待这里报错：Property 'age' is missing in type '{ name: string; }' but required in type '{ name: string; age: number; }'.
-    // add({ name: "david" });
-    //
-    // // 期待这里报错：Argument of type 'string' is not assignable to parameter of type 'number'.
-    // removeIndex("123");
+    client("users").then(setUsers);
   });
+
   return (
     <div>
-      {/*期待: 点击以后增加 john */}
-      <button onClick={() => add({ name: "john", age: 22 })}>add john</button>
-      {/*期待: 点击以后删除第一项*/}
-      <button onClick={() => removeIndex(0)}>remove 0</button>
-      {/*期待：点击以后清空列表*/}
-      <button style={{ marginBottom: "50px" }} onClick={() => clear()}>
-        clear
-      </button>
-      {value.map((person, index) => (
-        <div style={{ marginBottom: "30px" }} key={index}>
-          <span style={{ color: "red" }}>{index}</span>
-          <span>{person.name}</span>
-          <span>{person.age}</span>
-        </div>
-      ))}
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
     </div>
   );
 };
