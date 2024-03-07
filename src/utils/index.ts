9bcb1e995829c02cc21bcc09baacebaec656b30c
khs <khs@123.com>
2024-03-07 10:32:54 +0800

    chore: any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
 // 排除value为0
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown): boolean =>
+  value === 0 ? false : !value;
 
 export const cleanObject = (object: object) => {
   const result = { ...object };
@@ -23,7 +24,8 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+// 泛型规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
