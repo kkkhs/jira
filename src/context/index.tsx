63c4a281c26e2d0ac2026879497d86e21bab5cdb
khs <khs@123.com>
2024-03-08 14:36:47 +0800

    style: 优化项目列表页面

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,13 @@
 import React, { ReactNode } from "react";
 import { AuthProvider } from "context/auth-context";
+import { Query } from "@testing-library/react";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  // const queryClient = new QueryClient();
+
+  return (
+    // <QueryClientProvider client={queryClient}>
+    <AuthProvider>{children}</AuthProvider>
+    // </QueryClientProvider>
+  );
 };
