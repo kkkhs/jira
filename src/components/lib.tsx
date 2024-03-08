37e223bbbbe45c64e0835c48c92ff32d07053e4a
khs <khs@123.com>
2024-03-08 21:07:10 +0800

    feat: 处理me接口loading和error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 // emotion js 使用变量自定义样式
 export const Row = styled.div<{
@@ -23,3 +25,23 @@ export const Row = styled.div<{
           : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+  </FullPage>
+);
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,13 +1,6 @@
 import React, { ReactNode } from "react";
 import { AuthProvider } from "context/auth-context";
-import { Query } from "@testing-library/react";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
-  // const queryClient = new QueryClient();
-
-  return (
-    // <QueryClientProvider client={queryClient}>
-    <AuthProvider>{children}</AuthProvider>
-    // </QueryClientProvider>
-  );
+  return <AuthProvider>{children}</AuthProvider>;
 };
