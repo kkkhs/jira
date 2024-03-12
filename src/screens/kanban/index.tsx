6a25588d37f43129a1dec717fe2f94f3e57a0047
khs <khs@123.com>
2024-03-12 12:38:02 +0800

    feat: 创建看板与任务

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,32 +1,69 @@
 import React from "react";
 import { useDocumentTitle } from "utils";
-import { useKanbanSearchParams, useProjectInUrl } from "screens/kanban/util";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "screens/kanban/util";
 import { KanbanColumn } from "screens/kanban/kanban-column";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
 import { useKanbans } from "../../utils/kanban";
 import { ScreenContainer } from "../../components/lib";
+import { useTasks } from "../../utils/task";
+import { Spin } from "antd";
+import { CreateKanban } from "./create-kanban";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
 
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams(),
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = taskIsLoading || kanbanIsLoading;
+
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn kanban={kanban} key={kanban.id} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"}></Spin>
+      ) : (
+        <ColumnsContainer>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn kanban={kanban} key={kanban.id} />
+          ))}
+          <CreateKanban />
+        </ColumnsContainer>
+      )}
     </ScreenContainer>
   );
 };
 
-const ColumnsContainer = styled.div`
+export const ColumnsContainer = styled.div`
   display: flex;
-  // overflow-x: scroll;
+  overflow-x: scroll;
   flex: 1;
+
+  ::-webkit-scrollbar {
+    height: 1rem;
+    border-radius: 1rem;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  /*定义滚动条轨道
+     内阴影+圆角*/
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    border-radius: 1rem;
+    background-color: #f5f5f5;
+  }
+  /*定义滑块
+     内阴影+圆角*/
+  ::-webkit-scrollbar-thumb {
+    border-radius: 1rem;
+    -webkit-box-shadow: inset 0 0 2px rgba(0, 0, 0, 0.1);
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 `;
